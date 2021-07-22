999ea6e6a290ab6ab9edac5a9755fc0c95aa744e
GhosterAPI <77494104+GhosterAPI@users.noreply.github.com>
2021-07-22 16:02:18 +0200

    Update Main.java

--- Client Crasher/src/de/ghosterapi/Main.java
+++ Client Crasher/src/de/ghosterapi/Main.java
@@ -4,6 +4,13 @@ import de.ghosterapi.commands.CrashCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * @author Ghoster#0001
+ * @copyright Ghoster
+ * Created on 22.07.2021 «» 11:32
+ * Class «» Main
+ **/
+
 public class Main extends JavaPlugin {
 
     @Override
